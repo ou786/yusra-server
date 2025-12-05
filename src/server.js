931f30585cc2f7499cd3fd888c1fac5a9e509b13
ou786 <osamamohammed.ussaid@dentistryautomation.com>
2025-12-05 21:24:55 +0530

    Updated CORS with frontend

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ connectDB();
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://yusra-nine.vercel.app/",
   credentials: true,
 }));
 app.use(express.json());
